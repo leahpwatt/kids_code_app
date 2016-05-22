225764a05de93b229e109e7f3561dbce862c8fe2
Leah <leahpwatt@gmail.com>
2016-05-22 08:43:09 -0400

    api changes

--- controllers/hdCntrl.js
+++ controllers/hdCntrl.js
@@ -1,4 +1,15 @@
-angular.module("kidsCode")
-	.controller("hdCntrl", function(){
+angular.module('kidsCode')
+	.controller('testWord', function($http) {
+  
+// vulgar
+    $http.get("http://www.purgomalum.com/service/plain?text=jackass")
+        .then(function(response) {
+            console.log(response);
+        });
 
-	});
+// Not vulgar
+        $http.get("http://www.purgomalum.com/service/plain?text=test")
+        .then(function(response) {
+            console.log(response);
+        });
+});
