a05c130372d92b99d4459ae17a5d365355b22c22
Michael P. Johnson <mpjohnson_michigan@hotmail.com>
2016-05-23 11:50:56 -0400

    Updated hdCntrl

--- controllers/hdCntrl.js
+++ controllers/hdCntrl.js
@@ -1,15 +1,16 @@
-angular.module('kidsCode')
-	.controller('hdCntrl', function($http) {
-  
-// vulgar
-    $http.get("http://www.purgomalum.com/service/plain?text=jackass")
+var app = angular.module('kidsCode')
+    app.controller('hdCntrl', function($http) {
+    console.log("In hdCntrl");
+
+    // var htmlInput = "text";
+    var htmlInput = "jackass";
+    var text = "http://www.purgomalum.com/service/plain?text=" + htmlInput;
+    console.log(text);
+
+    $http.get(text)
         .then(function(response) {
-            console.log(response);
+            console.log(response.data);
+            return (response.data);
         });
 
-// Not vulgar
-        $http.get("http://www.purgomalum.com/service/plain?text=test")
-        .then(function(response) {
-            console.log(response);
-        });
 });
