0d0a0c652cd8ac407be2075a705f9ce658881ea0
Leah <leahpwatt@gmail.com>
2016-05-27 14:51:00 -0400

    fixed unwanted pop-up

--- controllers/inputCntrl4.js
+++ controllers/inputCntrl4.js
@@ -1,11 +1,13 @@
 angular.module("kidsCode")
-	.controller("inputCntrl4", function($scope, $rootScope, $location, checkInputService){
-	$scope.showExercise = function(input){
-			if(checkInputService.checkImgStyle(input)){ //runs input through compareParts function
-				$location.path("/exercise5"); //changing views
-			} else{
-				alert("Oops! Please try again.");
+	.controller("inputCntrl4", function($scope, $rootScope, vulServ, checkInputService, $location){
+			$scope.showExercise = function(str){
+				checkInputService.checkColor(str);
+				$location.path("/exercise5");
 		}
-	}
-
 	});
+
+
+
+
+
+	
--- controllers/inputCntrl5.js
+++ controllers/inputCntrl5.js
@@ -1,9 +1,6 @@
 angular.module("kidsCode")
 	.controller("inputCntrl5", function($scope, $rootScope, $location){
 		$scope.showExercise = function(str){
-			// accServ.printExercise(str);
-			$location.path("/exercise6");
-			console.log(str);
 		}
 
 
